fc3fa21ec25361bf852fe7ad29ed1f49e727d0d1
Fernando Mercado <fernan.alemercado@gmail.com>
2018-12-07 01:04:28 -0300

    formulario de login

---
+++ login.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="estilos/estilos.css">
+
+	<title>Facturación</title>
+</head>
+<body>
+	<div class="container-fluid" id="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<form action="" method="post">
+				<h3 class="bg-success">Iniciar Sesión</h3>
+				<img src="img/ingresar.png" alt="Login">
+				<input type="text" placeholder="Usuario" name="usuario" class="form-control mb-3 rounded">
+				<input type="password" name="password" placeholder="Contraseña" class="form-control mb-3 rounded">
+				<p class="alerta">Mensaje</p>
+				<input type="submit" class="btn btn-success btn-lg btn-block" value="Ingresar">
+
+				</form>
+			</div>
+			
+		</div>
+		
+		
+
+	</div>
+
+
+
+
+
+
+
+
+
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
